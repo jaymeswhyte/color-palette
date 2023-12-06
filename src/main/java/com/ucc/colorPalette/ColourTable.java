de578bca31b1050bd1b64aa34280bc96c7088386
jaymeswhyte <jmwhyte2002@gmail.com>
2023-12-06 12:54:02 +0000

    Added constructor. Test for exception with non-valid palette size failed

--- src/main/java/com/ucc/colorPalette/ColourTable.java
+++ src/main/java/com/ucc/colorPalette/ColourTable.java
@@ -1,5 +1,19 @@
 package com.ucc.colorPalette;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ColourTable {
+    private List<int[]> table;
+    // Constructor
+    public ColourTable(int numberOfColors) {
+        table = new ArrayList<>(numberOfColors);
+        initializeColorTable();
+    }
+
+    private void initializeColorTable() {
+        for (int i = 0; i < table.size(); i++) {
+            table.add(new int[]{0, 0, 0});
+        }
+    }
 
 }
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,2 +1,16 @@
+import com.ucc.colorPalette.ColourTable;
 public class test {
+    public static void main(String[] args) {
+        // A constructor for a ColourTable object with a single parameter that specifies the number of
+        //colours in the palette (the number of elements allowed in the colour lookup table
+        ColourTable table = new ColourTable(8);
+
+        // The number of colours in the palette must be a power of two and greater than 1 (e.g. 2, 4, 8…) and
+        //less than 1025
+        // Creating a ColourTable object without specifying a valid palette size throws an exception
+        ColourTable table2 = new ColourTable(7);
+        // A method add that enables a developer to add a 24-bit RGB colour to the colour table
+        // Exceeding the capacity of the ColourTable results in an exception
+    }
+
 }
