315674a1ceb3ebd89cf38f3a0fb4986df53f217e
jaymeswhyte <jmwhyte2002@gmail.com>
2023-12-06 14:07:39 +0000

    All tests and methods working as expected

--- src/main/java/com/ucc/colorPalette/ColourTable.java
+++ src/main/java/com/ucc/colorPalette/ColourTable.java
@@ -1,4 +1,5 @@
 package com.ucc.colorPalette;
+import javax.naming.SizeLimitExceededException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -6,7 +7,7 @@ public class ColourTable {
     private List<int[]> table;
     private int colourIndex = 0;
     // Constructor
-    public ColourTable(int numberOfColours) throws Exception {
+    public ColourTable(int numberOfColours) throws IllegalArgumentException {
         // The number of colours in the palette must be a power of two and greater than 1 (e.g. 2, 4, 8…) and
         //less than 1025
         if(numberOfColours>1 && numberOfColours<1025 && isPowerOfTwo(numberOfColours)){
@@ -14,7 +15,7 @@ public class ColourTable {
             initializeColourTable(numberOfColours);
         }
         else{
-            throw new Exception("Invalid Input");
+            throw new IllegalArgumentException("Invalid Input");
         }
 
     }
@@ -26,9 +27,14 @@ public class ColourTable {
     }
 
     // A method add that enables a developer to add a 24-bit RGB colour to the colour table
-    public void addColour(int[] newColour){
-        table.set(colourIndex, newColour);
-        colourIndex++;
+    public void addColour(int[] newColour)throws SizeLimitExceededException {
+        if(colourIndex < table.size()){
+            table.set(colourIndex, newColour);
+            colourIndex++;
+        }
+        else{
+            throw new SizeLimitExceededException("Table is full!");
+        }
     }
 
     private boolean isPowerOfTwo(int num) {
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,5 +1,6 @@
 import com.ucc.colorPalette.ColourTable;
 import org.junit.jupiter.api.Test;
+import javax.naming.SizeLimitExceededException;
 import static org.junit.jupiter.api.Assertions.*;
 public class test {
     static void testConstructorValidInput() {
@@ -8,46 +9,55 @@ public class test {
         try {
             ColourTable colourTable = new ColourTable(8);
             assertNotNull(colourTable);
+            System.out.println(0);
         }
         catch(Exception e){
-            System.out.println(e);
+            System.out.println(e.getMessage());
         }
     }
 
-    static void testConstructorInvalidInput(){
-        System.out.println("Testing Constructor with invalid input");
+    static void testConstructorInvalidInput() {
         // Creating a ColourTable object without specifying a valid palette size throws an exception
-        try{
-            ColourTable table = new ColourTable(7);
+        System.out.println("Testing Constructor with invalid input");
+        try {
+            assertThrows(IllegalArgumentException.class, () -> {
+                ColourTable table = new ColourTable(7);
+            }, "Expected IllegalArgumentException, but no exception was thrown");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
-        catch(Exception e){
-            System.out.println(e);
-        }
+        System.out.println(0);
     }
 
     static void testAddColour(){
-        System.out.println("Testing adding colour");
         // Test add colour method
+        System.out.println("Testing adding colour");
         try{
             ColourTable table = new ColourTable(8);
             table.addColour(new int[]{128, 128, 128});
+            System.out.println(0);
         }
         catch(Exception e){
-            System.out.println(e);
+            System.out.println(e.getMessage());
         }
     }
 
     static void testExceededSizeException(){
-        System.out.println("Testing exceeding palette size");
         // Test Exceeded Size Exception
-        try{
-            ColourTable table = new ColourTable(2);
+        System.out.println("Testing exceeding palette size");
+        ColourTable table = null;
+        try {
+            table = new ColourTable(2);
             table.addColour(new int[]{128, 128, 128});
             table.addColour(new int[]{128, 128, 128});
-            table.addColour(new int[]{128, 128, 128});
-        }
-        catch(Exception e){
-            System.out.println(e);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (table != null) {
+                ColourTable finalTable = table;
+                assertThrows(SizeLimitExceededException.class, () -> finalTable.addColour(new int[]{128, 128, 128}));
+            }
+            System.out.println(0);
         }
     }
     public static void main(String[] args){
